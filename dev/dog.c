d661e7ce517a0a39fdd16ebaf7f945269edba91e
Antônio Alexandre Cavalcante Leite <aacleite@gmail.com>
2026-01-25 16:54:42 -0300

    Atualização gitignore dog.c install.sh

--- dev/dog.c
+++ dev/dog.c
@@ -20,7 +20,7 @@ bool is_header = false;
 
 // NOTE: Função para escrever a versão do dog.
 bool print_version() {
-  FILE *file = fopen("version", "r");
+  FILE *file = fopen("/version", "r");
   if (file == NULL) {
     fprintf(stderr, "Falha ao executar o comando 'version'.\n");
     return true;
@@ -34,9 +34,9 @@ bool print_version() {
 
 // NOTE: Função para escrever o ajuda do dog.
 bool print_help() {
-  FILE *file = fopen("help", "r");
+  FILE *file = fopen("/help", "r");
   if (file == NULL) {
-    fprintf(stderr, "Falha ao executar o comando.\n");
+    fprintf(stderr, "Falha ao executar o comando 'help'.\n");
     return true;
   }
   char ch;
